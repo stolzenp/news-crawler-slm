d94b7c078967c72a98a659a6d407d672a0079941
stolzenp <stolzenp@student.hu-berlin.de>
2025-10-06 18:51:15 +0200

    update upload_model.py

--- evaluation/upload_model.py
+++ evaluation/upload_model.py
@@ -10,11 +10,11 @@ model_path = eval_args["model_name_or_path"]
 hf_model_path = eval_args["huggingface_model_path"]
 
 # load model and tokenizer
-model = AutoModelForCausalLM.from_pretrained("your_model_dir")
-tokenizer = AutoTokenizer.from_pretrained("your_model_dir")
+model = AutoModelForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # push model and tokenizer to Hugging Face Hub
-model.push_to_hub("your-username/your-model-name")
-tokenizer.push_to_hub("your-username/your-model-name")
+model.push_to_hub(hf_model_path)
+tokenizer.push_to_hub(hf_model_path)
 
-print(f"Model and Tokenizer uploaded to https://huggingface.co/models/{hf_model_path}")
+print(f"Model and Tokenizer uploaded to https://huggingface.co/{hf_model_path}")
